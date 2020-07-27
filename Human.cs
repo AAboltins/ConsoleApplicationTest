701d93a279d27b64e07b24b602aaa73a275610af
AAboltins <i.a.aboltins@gmail.com>
2020-07-27 18:39:34 +0300

    Add project files.

---
+++ Human.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Human
+    {
+        private bool End = false;
+        public string Name { get; }
+        public string Surname { get; }
+        public int Age { get; private set; }
+        public string EyeColor { get; }
+
+        public Human(string name, string surname, int age, string eyecolor)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.EyeColor = eyecolor; 
+
+        }
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, my full name is " + this.Name + " " + this.Surname + "." + "I am " + this.Age + " years old");
+        }
+        public void AddNumbers(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void SayName()
+        {
+            Console.WriteLine(this.Name);
+        }
+        public void SetAge(int age)
+        {
+            if (age < 0)
+            {
+                Console.WriteLine("The age can not be less than zero");
+                this.Age = age;
+            }
+            else if(age < this.Age)
+            {
+                Console.WriteLine ("The new age for " + this.Name + " is younger are you sure you want to change the age");
+                while (End == false)
+                {
+                    string answer = Console.ReadLine();
+                    if (answer == "yes" || answer == "Yes")
+                    {
+                        Console.WriteLine("ok");
+                        this.Age = age;
+                        End = true;
+                    }
+                    else if (answer == "no" || answer == "No")
+                    {
+                        Console.WriteLine("ok");
+                        End = true;
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("This is yes or no question");
+                        End = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Human> persons = new List<Human>();
+
+            
+            persons.Add(new Human("Jenifer", "Lopez", 13, "Brunas"));
+            persons.Add(new Human("Anna", "Skroderniece", 14, "Violetas"));
+            persons.Add(new Human("Mikelis", "Mucinieks", 16, "Zilas"));
+            persons.Add(new Human("Kristaps", "Kalejs", 17, "Zalas"));
+            persons[1].SetAge(2);
+
+             // persons[1] = new Human("ss", "Kla", 12, "zASDA");
+            //person[0].Introduce();
+            //person[1].Introduce();
+            //person[2].Introduce();
+            //person[3].Introduce();
+            //=
+            //|
+            //V
+            //for (int i = 0; i < persons.Count; i++)
+            //{
+            //    persons[i].Introduce();
+            //}
+            // = 
+            //|
+            //V
+            
+            foreach (var person in persons)
+            {
+                person.Introduce();
+            }
+        }
+      
+    }
+
+}
